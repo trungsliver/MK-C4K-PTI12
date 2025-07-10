536626a26623481399a99d9ca66e4f348bd66713
trungsliver <buiductrung.cva@gmail.com>
2025-07-10 16:52:01 +0700

    Lesson 5

---
+++ Lesson5/main.py
@@ -0,0 +1,40 @@
+import sys
+from PyQt6.QtWidgets import QMainWindow, QApplication
+from PyQt6 import QtCore, QtWidgets
+from PyQt6 import uic
+
+# xử lý
+app = QApplication(sys.argv)
+
+class Signup(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('ui//signup.ui',self)
+        # self.pushButton.clicked.connect(lambda: switch_window())
+
+    def login_check(self, name):
+        print(name)
+
+# Hàm hiển thị thông báo
+def msg_box(title, content):
+    msg = QtWidgets.QMessageBox()
+    msg.setStyleSheet("QLabel{min-width: 200px;}"
+                          "QLabel{max-width: 200px;}"
+                          "QMessageBox{background-color:rgba(35,36,40,255);}"
+                          "QPushButton{background-color:rgb(30,95,181);}"
+                          "QLabel{color:rgb(255,255,255);}"
+                          "QPushButton{color:rgb(255,255,255);}")
+    msg.setWindowTitle(title)
+    msg.setInformativeText(content)
+    msg.exec()
+
+# Chuyển cửa sổ giao diện
+def switch_window(classw):
+    global window
+    window = classw
+    window.show()
+
+# Run app
+window = Signup()
+window.show()
+sys.exit(app.exec())
